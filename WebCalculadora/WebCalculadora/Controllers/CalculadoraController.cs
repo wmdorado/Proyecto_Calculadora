381bff23d2cb0a3355c7d45635f1fc091ec709f4
wmdorado <a2017210277@estudiantes.upsa.edu.bo>
2021-01-23 20:07:26 +0100

    Resta Get y Post

--- WebCalculadora/WebCalculadora/Controllers/CalculadoraController.cs
+++ WebCalculadora/WebCalculadora/Controllers/CalculadoraController.cs
@@ -13,15 +13,32 @@ namespace WebCalculadora.Controllers
      
       
             [HttpGet]
+            [Route ("suma")]
             public int suma(int num1, int num2)
             {
                 return num1 + num2;
             }
             [HttpPost]
+            [Route("suma")]
             public int sumar([FromHeader] int num1, [FromHeader] int num2)
             {
                 return num1 + num2;
             }
-        
+
+        [HttpGet]
+        [Route("resta")]
+        public int resta(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        [HttpPost]
+        [Route("resta")]
+        public int restar([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 - num2;
+        }
+
+
+
     }
 }
